1ee30bbb6d5534d621684f4a07ce9806a862482d
Nadia <148998471+NadiaSuganiaka@users.noreply.github.com>
2024-11-13 08:57:19 +0200

    Update звязні списки.js

--- звязні списки.js
+++ звязні списки.js
@@ -39,8 +39,26 @@ search(value){
     }
     return null;
   }
+delete(value){
+  if (this.head === null){
+    return;
+  } 
+  if (this.head.value === value) {
+    this.head = this.head.next;
+    return;
+    
+  }
+  let current = this.head;
+    while(current.next !== null){
+      if (current.next.value === value) {
+        current.next = current.next.next;
+        return;
+      }
+      current = current.next;
+    }
+  }
 }
-  
+
 const linkedList = new LinkedList();
 console.log(linkedList);
 linkedList.add(1);
@@ -49,3 +67,5 @@ linkedList.prepend(2);
 console.log(linkedList);
 console.log(linkedList.search(1));
 console.log(linkedList.search(5));
+linkedList.delete(1);
+console.log(linkedList.search(1));
